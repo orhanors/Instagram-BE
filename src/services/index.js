0705b95f05d4b9bad06972c276d9c39c117bf74f
orhanors <orsorhan1@gmail.com>
2021-03-01 17:44:22 +0300

    enis post branch merged

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const services = require("express").Router();
 const { authRoute } = require("./auth");
+const { postRoute } = require("./post");
 services.use("/auth", authRoute);
+services.use("/posts", postRoute);
 
 module.exports = services;
--- src/services/post/post.controllers.js
+++ src/services/post/post.controllers.js
@@ -1,73 +1,76 @@
-const PostModel = require("./post.schema")
+const PostModel = require("./post.schema");
 
+exports.newPost = async (req, res, next) => {
+	try {
+		const imgUrl = req.file.path;
+		const newPost = await PostModel({ ...req.body, image: imgUrl });
+		const { _id } = newPost.save();
+		res.send(_id);
+	} catch (error) {
+		next(error);
+	}
+};
 
-exports.newPost = async(req,res,next)=>{
-    try {
-        const imgUrl = req.file.path
-        const newPost = await PostModel({...req.body,image:imgUrl})
-        const {_id} = newPost.save()
-        res.send(_id)
-    } catch (error) {
-        next(error)
-    }
-}
+exports.getSpecificPost = async (req, res, next) => {
+	try {
+		const specificPost = await PostModel.findById(req.params.postId);
+		res.status(200).send(specificPost);
 
+		console.log(specificPost);
+	} catch (error) {
+		next(error);
+	}
+};
 
-exports.getSpecificPost = async(req,res,next)=>{
-    try {
-       const specificPost = await PostModel.findById(req.params.postId)
-       res.status(200).send(specificPost)
-     
-        console.log(specificPost)
-    } catch (error) {
-        next(error)
-    }
-}
-
-exports.getAllMyPosts = async(req,res,next)=>{
-    try {
-        const myPost = await PostModel.findById
-    } catch (error) {
-        next(error)
-    }
-}
-exports.getAllPosts = async(req,res,next)=>{
-    try {
-       // this one is tricky with following . PENDING
-    } catch (error) {
-        next(error)
-    }
-}
-exports.editSinglePost = async(req,res,next)=>{
-    try {
-        if(req.file && req.file.path) {
-            const editPost = await PostModel.findByIdAndUpdate(req.params.postId,{
-                $set:{
-                    image:req.file.path
-                }
-            })
-            const {_id } = await editPost.save()
-            res.status(201).send(_id)
-        }else{
-            const editPost = await PostModel.findByIdAndUpdate(req.params.postId,{
-                $set:{
-                    description:req.body.description
-                }
-            })
-            const {_id} = await editPost.save()
-            res.status(201).send(_id)
-        }
-    
-        
-    } catch (error) {
-        next(error)
-    }
-}
-exports.deletePost = async(req,res,next)=>{
-    try {
-        const deletePost = await PostModel.findByIdAndDelete(req.params.postId)
-        res.status(204).send("Deleted")
-    } catch (error) {
-        next(error)
-    }
-}
+exports.getAllMyPosts = async (req, res, next) => {
+	try {
+		const myPost = await PostModel.findById;
+	} catch (error) {
+		next(error);
+	}
+};
+exports.getAllPosts = async (req, res, next) => {
+	try {
+		// this one is tricky with following . PENDING
+		res.send("Ok");
+	} catch (error) {
+		next(error);
+	}
+};
+exports.editSinglePost = async (req, res, next) => {
+	try {
+		if (req.file && req.file.path) {
+			const editPost = await PostModel.findByIdAndUpdate(
+				req.params.postId,
+				{
+					$set: {
+						image: req.file.path,
+					},
+				}
+			);
+			const { _id } = await editPost.save();
+			res.status(201).send(_id);
+		} else {
+			const editPost = await PostModel.findByIdAndUpdate(
+				req.params.postId,
+				{
+					$set: {
+						description: req.body.description,
+					},
+				}
+			);
+			const { _id } = await editPost.save();
+			res.status(201).send(_id);
+		}
+	} catch (error) {
+		next(error);
+	}
+};
+exports.deletePost = async (req, res, next) => {
+	try {
+		const deletePost = await PostModel.findByIdAndDelete(req.params.postId);
+		res.status(204).send("Deleted");
+	} catch (error) {
+		next(error);
+	}
+};
