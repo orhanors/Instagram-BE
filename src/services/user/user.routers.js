be8d8fc3dd696bfe4e47edd19904a65e655d5947
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-05 07:11:39 +0100

    adds route to update image

--- src/services/user/user.routers.js
+++ src/services/user/user.routers.js
@@ -1,4 +1,5 @@
 const userRouter = require("express").Router();
+const cloudinaryMulter = require("../../middlewares/cloudinary");
 
 const { validateToken } = require("../../middlewares/validateToken");
 const {
@@ -10,6 +11,7 @@ const {
 	getNotifications,
 	changeNotificationSeen,
 	editUserProfile,
+	editUserImage,
 } = require("./user.controllers");
 
 userRouter.get("/me", validateToken, getUserProfile);
@@ -31,5 +33,11 @@ userRouter.put(
 	validateToken,
 	changeNotificationSeen
 );
+
+userRouter.put("/me/update/image", 
+cloudinaryMulter.single("image"),
+validateToken, 
+editUserImage
+)
 
 module.exports = userRouter;
